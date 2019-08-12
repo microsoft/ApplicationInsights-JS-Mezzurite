a40c32bafea492f464e33c70bdd01144b2b69f96
C. Naoto Abreu Takemura <caabreut@microsoft.com>
2019-08-12 03:59:34 -0700

    Wrapped the functionality in index.js into a class: MezzuriteAppInsightsLogger in order to encapsulate the add/remove listener implementation from the user, as the remove operation requires the listener function to remove.

---
+++ src/MezzuriteAppInsightsLogger.js
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import onComponentsChanged from './onComponentsChanged';
+
+class MezzuriteAppInsightsLogger {
+  constructor(ai) {
+    this.listener = e => onComponentsChanged(e, ai);
+  }
+
+  initialize() {
+    window.addEventListener('mezzurite/componentsChanged', this.listener);
+  }
+
+  dispose() {
+    window.removeEventListener('mezzurite/componentsChanged', this.listener);
+  }
+}
+
+export default MezzuriteAppInsightsLogger;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import onComponentsChanged from './onComponentsChanged';
+import MezzuriteAppInsightsLogger from './MezzuriteAppInsightsLogger';
 
-function initialize (ai) {
-  const listener = e => onComponentsChanged(e, ai);
-  window.addEventListener('mezzurite/componentsChanged', listener);
-  return listener;
-}
-
-function dispose (listener) {
-  window.removeEventListener('mezzurite/componentsChanged', listener);
-}
-
-export default {
-  initialize,
-  dispose
-};
+export default MezzuriteAppInsightsLogger;
